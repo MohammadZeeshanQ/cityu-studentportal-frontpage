d4d2d6fd684d1877624b1a3049c8b8e3576bb6bf
MohammadZeeshanQ <zaqil1976@gmail.com>
2021-04-20 09:46:53 +0300

    Added: carousel and Text on new portal

--- src/pages/components/NewPortal.js
+++ src/pages/components/NewPortal.js
@@ -1,9 +1,132 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import {Typography, Button} from '@material-ui/core';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+// importing images from assest
+import Background1 from '../../Assets/Images/Slider1.jpg';
+import Background2 from '../../Assets/Images/Slider2.jpg';
+import Background3 from '../../Assets/Images/Slider3.jpg';
+
+const useStyles = makeStyles({
+    root:{
+        width:'100vw',
+        height:'100vh',
+        border: '1px red solid',
+    },
+    wrapper:{
+
+    },
+    sliderContainer:{
+        
+
+    },
+    sliderContentDiv:{
+        position: 'relative',
+        width: '100%',
+        height:'100%',
+    },
+    sliderBackgroundImage:{
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '100%',
+        height:'100%',
+        objectFit: 'cover',
+        ZIndex:'-1',
+    },
+    contentDiv:{
+        textAlign: 'center',
+        //padding: '10em 0',
+        border: '1px red solid'
+    }
+
+});
 
 export default function NewPortal() {
+
+    const classes = useStyles();
+
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        swipeToSlide: false,
+        swipe: false,
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              initialSlide: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+
     return (
-        <div>
-            
+        <div className={classes.root}>
+
+            <div className={classes.contentDiv} >
+
+                    <Typography variant='h5' className={classes.contentText} gutterBottom>
+                        Welcome to
+                    </Typography>
+
+                    <Typography variant='h2' className={classes.contentText} gutterBottom>
+                        Student Portal
+                    </Typography>
+                    
+                    <div className={classes.btnDiv} >
+                        <Button variant='contained' className={classes.btnDirect} >Login Now</Button>
+                    </div>
+
+            </div>
+
+            <div className={classes.wrapper}>
+
+                <Slider {...settings} className={classes.sliderContainer}>
+
+                    <div className={classes.sliderContentDiv}>
+                        <img className={classes.sliderBackgroundImage} src={Background1} alt='Background People'/>
+                    </div>
+
+                    <div className={classes.sliderContentDiv}>
+                        <img className={classes.sliderBackgroundImage} src={Background2} alt='Background People'/>
+                    </div>
+
+                    <div className={classes.sliderContentDiv}>
+                        <img className={classes.sliderBackgroundImage} src={Background3} alt='Background People'/>
+                    </div>
+
+                </Slider>
+
+               
+                
+            </div>
         </div>
     )
 }
