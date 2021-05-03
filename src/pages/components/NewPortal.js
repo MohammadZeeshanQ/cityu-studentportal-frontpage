af8500a1fdc5505adeb69464beaf1951c3a0c087
Zeeshan <zeeshan@policystreet.com>
2021-05-03 07:07:05 +0300

    Flag

--- src/pages/components/NewPortal.js
+++ src/pages/components/NewPortal.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core';
+import Slider from "react-slick";
+
+const useStyles = makeStyles(()=> ({
+    root:{
+
+    },
+    wrapper:{
+
+    },
+
+}));
 
 export default function NewPortal() {
+
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        
+      };
+
     return (
-        <div>
-            
+        <div className={classes.root}>
+            <div className={classes.wrapper}>
+                <Slider {...settings}>
+
+                </Slider>
+            </div>
         </div>
     )
 }
